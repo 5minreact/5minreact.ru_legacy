6607a73f26a7addeb9ba6d982f2d258943a357ee
Petr Myazin <petr.myazin@gmail.com>
2017-05-04 10:42:46 +0300

    Remove font from google

--- html.js
+++ html.js
@@ -12,8 +12,7 @@ module.exports = React.createClass({
     render() {
         const {body, route} = this.props
         const title = DocumentTitle.rewind()
-        const font = <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
-        let css
+        let css;
         if (process.env.NODE_ENV === 'production') {
             css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
         }
@@ -32,7 +31,6 @@ module.exports = React.createClass({
               <title>
                 { title }
               </title>
-              { font }
               { css }
             </head>
             <body>
