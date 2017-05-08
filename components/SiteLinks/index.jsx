338dbd329e69652d6d3cc4ae32a1fbd458149dd6
Petr Myazin <petr.myazin@gmail.com>
2017-05-08 16:51:23 +0500

    #14 - Redux Business Logic (part 2)

--- components/SiteLinks/index.jsx
+++ components/SiteLinks/index.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import { RouteHandler, Link } from 'react-router'
-import { prefixLink } from 'gatsby-helpers'
-import { config } from 'config'
-import './style.css'
-import '../../static/fonts/fontawesome/style.css'
+import React from "react";
+import {RouteHandler} from "react-router";
+import {prefixLink} from "gatsby-helpers";
+import {config} from "config";
+import "./style.css";
+import "../../static/fonts/fontawesome/style.css";
 
 class SiteLinks extends React.Component {
     render() {
 
         return (
             <div className='blog-social'>
-              <ul>
-                <li>
-                  <a href={ config.siteTwitterUrl } target="_blank"><i className='fa fa-twitter'></i></a>
-                </li>
-                <li>
-                  <a href={ config.siteEmailUrl }><i className='fa fa-envelope-o'></i></a>
-                </li>
-                <li>
-                  <a href={ config.siteRssUrl } target="_blank"><i className='fa fa-rss'></i></a>
-                </li>
-                <li>
-                  <a href={ config.siteItunesUrl } target="_blank"><i className='fa fa-podcast'></i></a>
-                </li>
-              </ul>
-              <p>
-              <small>Послушайте также: <a href="http://5minphp.ru" target="_blank">Пятиминутка PHP</a></small>
-              <small>Автор: <a href={ config.authorTwitterUrl } target="_blank">@PetrMyazin</a></small>
-              </p>
+                <ul>
+                    <li>
+                        <a href={ config.siteTwitterUrl } target="_blank"><i className='fa fa-twitter'></i></a>
+                    </li>
+                    <li>
+                        <a href={ config.siteEmailUrl }><i className='fa fa-envelope-o'></i></a>
+                    </li>
+                    <li>
+                        <a href={ config.siteRssUrl } target="_blank"><i className='fa fa-rss'></i></a>
+                    </li>
+                    <li>
+                        <a href={ config.siteItunesUrl } target="_blank"><i className='fa fa-podcast'></i></a>
+                    </li>
+                </ul>
+                <p>
+                    <small>Listen in English: <a href="https://5minreact.audio"
+                                                 target="_blank">https://5minreact.audio</a></small>
+                    <small>Послушайте также: <a href="http://5minphp.ru" target="_blank">Пятиминутка PHP</a></small>
+                    <small>Автор: <a href={ config.authorTwitterUrl } target="_blank">@PetrMyazin</a></small>
+                </p>
             </div>
-            );
+        );
     }
 }
 
---
+++ pages/articles/2017-05-08-redux-business-logic-part2/index.md
@@ -0,0 +1,15 @@
+---
+title: "#14 - Redux Business Logic (part 2)"
+date: "2017-05-08"
+layout: post
+path: "/14-redux-business-logic-part2/"
+---
+
+<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/321546121&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+
+В предыдущем выпуске подкаста мы разбирались с вопросом где разместить бизнес логику при использовании Redux. Но остались острые вопросы, которые я не успел осветить. Углубимся в тему ещё немного!
+
+- http://redux.js.org/docs/faq/CodeStructure.html#how-should-i-split-my-logic-between-reducers-and-action-creators-where-should-my-business-logic-go
+- http://redux.js.org/docs/recipes/ComputingDerivedData.html
+- https://egghead.io/lessons/javascript-redux-colocating-selectors-with-reducers - Building React Applications with Idiomatic Redux
+- https://twitter.com/dan_abramov/status/664581975764766721 - redux tip: export selectors with related reducers
